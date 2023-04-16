7b35df77a48a0b84aa814bd107e5a7da674a0223
wallyson <wallysom2@gmail.com>
2023-04-16 17:28:01 -0300

    feat (tickets): create controller getTickets

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import getTicketTypes from '@/services/tickets-service';
+import { AuthenticatedRequest } from '@/middlewares';
 
 export async function getTicketTypes(req: Request, res: Response) {
-
   try {
     const ticketTypes = await ticketService.getTicketTypes();
-    return res.status(httpStatus.OK).json(ticketTypes);
+    return res.status(httpStatus.OK).send(ticketTypes);
     }
   catch (error) {
-    return res.status(httpStatus.NOT_FOUND).send({});
+    return res.status(httpStatus.NOT_FOUND);
   }
+}
 
+export async function getTickets(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  try {
+    const tickets = await ticketService.getTickets(userId);
+    return res.status(httpStatus.OK).send(tickets);
+  }
+  catch (error) {
+    return res.status(httpStatus.NOT_FOUND);
+  }
 }
